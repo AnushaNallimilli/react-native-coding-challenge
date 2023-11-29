5c7f21265565ce64d605e37fda5215ecc8b7c50f
Anusha <anallimill@kcftech.com>
2023-11-29 20:08:54 +0530

    commit

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ import App from './src/App';
 import {name as appName} from './app.json';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from 'react-query';
-//import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const queryClient = new QueryClient();
-//AsyncStorage.clear();
 const AppComponent = () => (
   <QueryClientProvider client={queryClient}>
     <NavigationContainer>
